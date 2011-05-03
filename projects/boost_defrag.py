976b786738965777dbf29e4febadcfbba76edc88
Dave Abrahams <dave@boostpro.com>
2011-05-03 17:09:08 -0400

    Hew much more closely to the BuildBot way of doing things

--- projects/boost_defrag.py
+++ projects/boost_defrag.py
@@ -10,79 +10,48 @@ import buildbot.process.properties
 from buildbot import util
 from collections import Callable
 
-class WithProperties(buildbot.process.properties.WithProperties):
-    """
-    This is a marker class, used fairly widely to indicate that we
-    want to interpolate build properties.
-    """
+from buildbot.process.properties import WithProperties
 
-    compare_attrs = ('fmtstring', 'args', 'kw')
+class interpolated(object):
+    def __init__(self, fn):
+        self.fn = fn
 
-    def __init__(self, fmtstring, *args, **kw):
-        self.fmtstring = fmtstring
-        self.args = args
-        self.kw = kw
-        if args and kw:
-            raise ValueError('WithProperties takes either positional or keyword substitutions, not both.')
+    def __get__(self, obj, objtype):
+        return objtype('%%(%s)s' % self.fn.__name__)
+    
+class Portable(WithProperties):
+    
+    def __init__(self, fmt, **kw):
+        kw = kw.copy()
+        kw.update(self.__class__.__dict__)
+        WithProperties.__init__(self, fmt, kw)
 
-    def __repr__(self):
-        return self.__class__.__name__+'(%(fmtstring)r, *%(args)r, **%(kw)r)' % self.__dict__
+    @interpolated
+    def setup(p):
+        if p['os'].startswith('win'):
+            m = re.match(r'vc([0-9]+)(?:\.([0-9]))?', p['cc'])
+            if m:
+                return r'${VS%s%sCOMNTOOLS}\vsvars32' % (m.group(1), m.group(2) or '0')
+            else:
+                return 'title'
+        return 'true'
 
-    def render(self, pmap):
-        if self.args:
-            strings = []
-            for name in self.args:
-                strings.append(pmap[name])
-            s = self.fmtstring % tuple(strings)
-        else:
-            for k,v in self.kw.iteritems():
-                pmap.add_temporary_value(k, v)
+    @interpolated
+    def make(p):
+        return p['os'].startswith('win') and 'nmake' or 'make'
 
-            properties = pmap.properties()
-            print '#######', 
-            import pprint
-            pprint.pprint(properties.asDict())
-            for k,v in properties.asDict().iteritems():
-                if isinstance(v, Callable):
-                    pmap.add_temporary_value(k, v(properties))
-            s = self.fmtstring % pmap
-            pmap.clear_temporary_values()
-        return s
+    @interpolated
+    def make_continue_opt(p):
+        return p['os'].startswith('win') and '-k' or '/K'
 
-def toolchain_setup(p):
-    if p['os'].startswith('win'):
-        m = re.match(r'vc([0-9]+)(?:\.([0-9]))?', p['cc'])
-        if m:
-            return r'${VS%s%sCOMNTOOLS}\vsvars32' % (m.group(1), m.group(2) or '0')
-        else:
-            return 'title'
-    return 'true'
-
-portability_properties = dict(
-    make=lambda p: p['os'].startswith('win') and 'nmake' or 'make',
-    make_k=lambda p: p['os'].startswith('win') and '-k' or '/K',
-    toolchain_setup=toolchain_setup,
-    )
+    @interpolated
+    def nil(p):
+        return p['os'].startswith('win') and 'echo' or 'true'
 
 def variant_properties(variant):
     return dict(
         src=lambda _:'variant'=='Debug' and '../source' or '../debug/monolithic',
         variant=variant)
-
-def propertize(stepClass, properties):
-    class Propertized(stepClass):
-        __properties=properties
-        
-        def start(self):
-            for k,v in self.__properties.iteritems():
-                # Allow property values from earlier to be used here
-                # but don't overwrite any rendered stuff
-                if not isinstance(v, Callable):
-                    self.setProperty(k, v)
-            self.setCommand
-            return stepClass.start(self)
-
-    return Propertized
             
 class DefragTests(BuildProcedure):
     def __init__(self, repo):
@@ -100,20 +69,23 @@ class DefragTests(BuildProcedure):
                 description='Documentation'))
 
     def test(self, variant):
-        props = variant_properties(variant)
-        props.update(portability_properties)
+        setup = [ Portable.setup, '&&' ]
 
-        toolchain = [ WithProperties('%(toolchain_setup)s'), '&&' ]
         self.addSteps(
-            SetProperty(command = ['echo'], extract_fn=lambda status,out,err: props),
+            SetProperty(
+                command = [Portable.nil],
+                extract_fn=lambda status,out,err: { 
+                    'variant':variant, 
+                    'src': (variant == 'Debug' and '../source' or '../debug/monolithic'),
+                    }),
 
             Configure(
-                command = toolchain + [ 'cmake', '-DBOOST_UPDATE_SOURCE=1',
-                            '-DBOOST_DEBIAN_PACKAGES=1', 
-                            WithProperties('-DCMAKE_BUILD_TYPE=%(variant)s') ] ),
+                command = setup + [
+                    'cmake', '-DBOOST_UPDATE_SOURCE=1', '-DBOOST_DEBIAN_PACKAGES=1', 
+                    '-DCMAKE_BUILD_TYPE='+variant, WithProperties('%(src)s') ] ),
 
-            Compile(command = toolchain + [ WithProperties('%(make)s'), WithProperties('%(make_k)s') ]),
-            Test(command = toolchain + [ WithProperties('%(make)s'), WithProperties('%(make_k)s'), 'test' ]),
+            Compile(command = setup + [ Portable.make, Portable.make_continue_opt ]),
+            Test(command = setup + [ Portable.make, Portable.make_continue_opt, 'test' ]),
             )
 
 
