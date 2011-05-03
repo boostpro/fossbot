7f40c81ff6600a5ab4cd514e14a2c8fda61bc007
Dave Abrahams <dave@boostpro.com>
2011-05-03 16:14:01 -0400

    New approach: try to use SetProperty

--- projects/boost_defrag.py
+++ projects/boost_defrag.py
@@ -4,7 +4,7 @@ from fossbot.bbot.status import IRC, MailNotifier
 from fossbot.bbot.memoize import memoize
 
 from buildbot.steps.source import Git
-from buildbot.steps.shell import Configure, Compile, Test, ShellCommand
+from buildbot.steps.shell import Configure, Compile, Test, ShellCommand, SetProperty
 
 import buildbot.process.properties
 from buildbot import util
@@ -92,45 +92,32 @@ class DefragTests(BuildProcedure):
             Git('http://github.com/%s.git' % repo, workdir='source'))
 
         self.test('Debug')
-
         self.test('Release')
 
-        self.command(
-            ShellCommand, 'Release',
-            command = ['make', 'documentation', '-k'],
-            description='Documentation')
+        self.step(
+            ShellCommand(
+                command = ['make', 'documentation', '-k'],
+                description='Documentation'))
 
-    def command(self, cls, variant, command, **kw):
-        
+    def test(self, variant):
         props = variant_properties(variant)
         props.update(portability_properties)
 
-        step = propertize(cls, props)(
-            command = [WithProperties('%(toolchain_setup)s', **props), '&&'] + command,
-            workdir = variant,
-            **kw)
+        self.addSteps(
+            SetProperty(extract_fn=lambda status,out,err: props),
 
-        step.description += ' (%s)' % variant
-           
-        self.step(step)
+            Configure(
+                command = [ 'cmake', '-DBOOST_UPDATE_SOURCE=1',
+                            '-DBOOST_DEBIAN_PACKAGES=1', 
+                            WithProperties('-DCMAKE_BUILD_TYPE=%(variant)s') ] ),
 
-    def test(self, variant):
-        self.command(
-            Configure, variant,
-            command = [ 'cmake', '-DBOOST_UPDATE_SOURCE=1',
-                        '-DBOOST_DEBIAN_PACKAGES=1', 
-                        WithProperties('-DCMAKE_BUILD_TYPE=%(variant)s') ] )
-        self.command(
-            Compile, variant,
-            command = [ WithProperties('%(make)s'), WithProperties('%(make_k)s') ])
-
-        self.command(
-            Test, variant,
-            command = [ WithProperties('%(make)s'), WithProperties('%(make_k)s') ])
+            Compile(command = [ WithProperties('%(make)s'), WithProperties('%(make_k)s') ]),
+            Test(command = [ WithProperties('%(make)s'), WithProperties('%(make_k)s'), 'test' ]),
+            )
 
 
-hub_repo = 'ryppl/Boost.Defrag'
 name = 'Boost.Defrag'
+hub_repo = 'ryppl/' + name
 
 
 include_properties=['os', 'cc']
