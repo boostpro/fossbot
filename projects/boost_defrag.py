e9f34697f2ab2cf28b215d3258e206fa510c05bb
Dave Abrahams <dave@boostpro.com>
2011-05-04 18:26:50 -0400

    Give in and use real shell commands so we can use "&&" after setting up the environment

--- projects/boost_defrag.py
+++ projects/boost_defrag.py
@@ -19,99 +19,90 @@ class interpolated(object):
 
     def __get__(self, obj, objtype):
         return objtype('%%(%s)s' % self.fn.__name__)
-    
+
+def make(p):
+    return p['os'].startswith('win') and 'nmake' or 'make'
+
+def make_continue_opt(p):
+    return p['os'].startswith('win') and '/K' or '-k' 
+
+def nil(p):
+    return p['os'].startswith('win') and 'rem' or 'true'
+
+def shell(p):
+    return p['os'].startswith('win') and 'cmd' or 'sh'
+
+def shell_cmd_opt(p):
+    return p['os'].startswith('win') and '/c' or '-c'
+
 class Portable(WithProperties):
     def __init__(self, fmt, **kw):
         kw = kw.copy()
+
         for name in re.findall(r'(?<!%)%[(]([A-Za-z_]\w*)[)]', fmt):
-            interp = self.__class__.__dict__.get(name, None)
-            if interp: 
-                kw[name] = interp.fn
+            f = self.__class__.__dict__.get(name, None)
+            if f: kw[name] = f
 
         WithProperties.__init__(self, fmt, **kw)
 
-    @interpolated
-    def get_toolset_environ(p):
-        if p['os'].startswith('win'):
-            m = re.match(r'vc([0-9]+)(?:\.([0-9]))?', p['cc'])
-            if m:
-                return r'${VS%s%sCOMNTOOLS}\vsvars32 && python -c "import os;print os.environ"' % (m.group(1), m.group(2) or '0')
-            else:
-                return 'rem'
-        return 'true'
+    def tool_env(p):
+        m = re.match(r'vc([0-9]+)(?:\.([0-9]))?', p.get('cc',''))
+        if m:
+            return r'${VS%s%sCOMNTOOLS}\vsvars32 &&' % (m.group(1), m.group(2) or '0')
+        return ''
 
-    @interpolated
     def make(p):
         return p['os'].startswith('win') and 'nmake' or 'make'
 
-    @interpolated
     def make_continue_opt(p):
         return p['os'].startswith('win') and '/K' or '-k' 
 
-    @interpolated
     def nil(p):
         return p['os'].startswith('win') and 'echo' or 'true'
 
-    @interpolated
     def shell(p):
         return p['os'].startswith('win') and 'cmd' or 'sh'
 
-    @interpolated
     def shell_cmd_opt(p):
         return p['os'].startswith('win') and '/c' or '-c'
+
+    def slash(p):
+        return p['os'].startswith('win') and '\\' or '/'
 
 
 class DefragTests(BuildProcedure):
     def __init__(self, repo):
         BuildProcedure.__init__(self, 'Boost.Defrag')
 
-        self.addSteps(
-            Git('http://github.com/%s.git' % repo, workdir='source'),
-
-            SetProperty(
-                description = 'Toolset setup',
-                command = [ Portable.shell, Portable.shell_cmd_opt, Portable.get_toolset_environ ],
-                extract_fn=
-                  lambda status,out,err: dict( toolset_environ=eval(out.strip() or '{}') )),
-            )
-
         self.test('Debug')
         self.test('Release')
 
         self.step(
             ShellCommand(
-                env=WithProperties('%(toolset_environ)s'),
                 workdir='Release',
-                command = [Portable.make, 'documentation', '-k'],
+                command = Portable('%(tool_env)s %(make)s %(make_continue_opt)s documentation'),
                 description='Documentation'))
+
+    @staticmethod
+    def __srcdir(variant):
+        return 
 
     def test(self, variant):
         self.addSteps(
-            SetProperty(
-                description = 'Variant setup',
-                command = [Portable.nil],
-                extract_fn=lambda status,out,err: { 
-                    'variant':variant, 
-                    'src': (variant == 'Debug' and '../source' or '../Debug/monolithic'),
-                    }),
-
             Configure(
-                env=WithProperties('%(toolset_environ)s'),
                 workdir=variant,
-                command = [
-                    'cmake', '-DBOOST_UPDATE_SOURCE=1', '-DBOOST_DEBIAN_PACKAGES=1', 
-                    '-DCMAKE_BUILD_TYPE='+variant, WithProperties('%(src)s') ] ),
+                command = Portable(
+                    '%(tool_env)s cmake -DBOOST_UPDATE_SOURCE=1'
+                    ' -DBOOST_DEBIAN_PACKAGES=1 "-DCMAKE_BUILD_TYPE='+variant+'" '
+                    + variant == 'Debug' and '..%(slash)ssource' or '..%(slash)sDebug%(slash)smonolithic')),
 
             Compile(
-                env=WithProperties('%(toolset_environ)s'),
                 workdir=variant, 
-                command = [ Portable.make, Portable.make_continue_opt ]),
+                command = Portable('%(tool_env)s %(make)s %(.make_continue_opt)')),
 
             Test(
-                env=WithProperties('%(toolset_environ)s'),
                 workdir=variant, 
-                command = [ Portable.make, Portable.make_continue_opt, 'test' ]),
-            )
+                command = Portable('%(tool_env)s %(make)s %(.make_continue_opt) test ')))
 
 
 name = 'Boost.Defrag'
