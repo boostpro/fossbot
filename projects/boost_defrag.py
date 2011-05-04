b5eb8345c9ce8f6b6f39a01e199138acbe0cead6
Dave Abrahams <dave@boostpro.com>
2011-05-04 09:53:02 -0400

    Another attempt to get the toolset environment thing to work

--- projects/boost_defrag.py
+++ projects/boost_defrag.py
@@ -62,7 +62,7 @@ class Portable(WithProperties):
 
     @interpolated
     def toolset_environ(p):
-        return p['toolset_environ']
+        return p['tool_environ']
 
 
 class DefragTests(BuildProcedure):
@@ -76,7 +76,7 @@ class DefragTests(BuildProcedure):
                 description = 'Toolset setup',
                 command = [ Portable.shell, Portable.shell_cmd_opt, Portable.get_toolset_environ ],
                 extract_fn=
-                  lambda status,out,err: dict( toolset_environ=eval(out.strip() or '{}') )),
+                  lambda status,out,err: dict( tool_environ=eval(out.strip() or '{}') )),
             )
 
         self.test('Debug')
