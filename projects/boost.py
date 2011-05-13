a2477fba09c1390db97ddca61a3418d5d2159179
Dave Abrahams <dave@boostpro.com>
2011-05-13 08:10:17 -0600

    boost.defrag becomes boost, testing via the new supermodule

--- projects/boost.py
+++ projects/boost.py
@@ -37,19 +37,18 @@ def cmake(step):
 
 class DefragTests(BuildProcedure):
     def __init__(self, repo):
-        BuildProcedure.__init__(self, 'Boost.Defrag')
+        BuildProcedure.__init__(self, 'Integration Test')
 
         self.addSteps(
             Git(repourl='git://github.com/%s.git' % repo, haltOnFailure=True),
-            ShellCommand(command=cmake('aggregate'), name='Collect Modules', haltOnFailure=True, alwaysRun=True),
             Configure(command=cmake('configure'), haltOnFailure=True, alwaysRun=True),
             Compile(command=cmake('build'), haltOnFailure=True, alwaysRun=True),
             Test(command=cmake('test'), haltOnFailure=True, alwaysRun=True),
             ShellCommand(command=cmake('package'), name='Package'))
 
 
-name = 'Boost.Defrag'
-hub_repo = 'ryppl/' + name
+name = 'Boost'
+hub_repo = 'boost-lib/boost-supermodule'
 
 include_features=['os', 'cc']
 
